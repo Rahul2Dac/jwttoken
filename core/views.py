d785b2df75eb04a8daefddc255dc783354ee79b1
Rahul Pr <rahuldac4141@gmail.com>
2020-09-06 05:49:36 -0700

    jwt

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated 
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)             # <-- And here
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
+
+
+
